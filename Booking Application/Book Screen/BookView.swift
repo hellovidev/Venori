5d2ffb90acd6df5f1eda5a2e0ae953dcbe330278
hellovidev <hellovidev@gmail.com>
2021-04-19 13:46:40 +0300

    Book View Complete

--- Booking Application/Book Screen/BookView.swift
+++ Booking Application/Book Screen/BookView.swift
@@ -8,72 +8,176 @@
 import SwiftUI
 
 struct BookView: View {
+    @State private var isComplete: Bool = false
+    
     var body: some View {
-        VStack{
-            if true {
-        Text("Book")
-        
-        Text("Reservation ID")
-        Text("121231")
-        
-        HStack{
-        VStack {
-        Text("Expected date of arrival")
-        Text("Today")
-        }
-            Image(systemName: "celender")
-        }
-        
-        HStack{
-        VStack {
-        Text("Expected time of arrival")
-        Text("16:55")
-        }
-            Image(systemName: "clock")
-        }
-        
-        Text("Number of adults")
-//        Stepper(value: 1, in: 5) {
-//            Text("1")
-//        }
-        
-        Text("Is this a Special Event?")
-//        Toggle(isOn: false) {
-//            Text("Hi")
-//        }
-        
-            Text("How long will you be dining with us?")
-//        Stepper(value: 1, in: 5) {
-//            Text("1")
-//        }
-        
-            Text("Available reservation times")
-        //List {}
-        
-        //Button {} label: { Text("Continue") }
-            
-            
-            
-            Button(action: {
-                //loginViewModel?.tryLogin()
-            }) {
-                Text("Continue")
-                    .foregroundColor(.white)
-                    .font(.system(size: 17, weight: .semibold))
-                    .padding(.top, 13)
-                    .padding(.bottom, 13)
-                    .padding(.leading, 16)
-                    .padding(.trailing, 16)
-                    .frame(maxWidth: .infinity)
-                    .shadow(radius: 10)
+        VStack(alignment: .leading) {
+            if !isComplete {
+                BookProcessView(actionContinue: {
+                    self.isComplete.toggle()
+                }, actionClose: {
+                    // Action Close View
+                })
+            } else {
+                CompleteView(actionContinue: {
+                    // Continue Action
+                }, actionBack: {
+                    self.isComplete.toggle()
+                })
             }
-            .modifier(ButtonModifier())
-        } else {
-            Image(systemName: "Complete")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct CompleteView: View {
+    var actionContinue: () -> Void
+    var actionBack: () -> Void
+    
+    var body: some View {
+        Button {
+            self.actionBack()
+        } label: {
+            Image("Arrow Left")
+                .padding([.top, .bottom, .trailing], 22)
+                .padding(.leading, 16)
+        }
+        VStack(alignment: .center) {
+            Image("Complete")
+                .padding(.bottom, 65)
             Text("The table has been booked")
+                .font(.system(size: 22, weight: .bold))
+                .padding(.bottom, 16)
             Text("We’ll wait for you on time.")
+                .font(.system(size: 20, weight: .regular))
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        Spacer()
+        Button(action: {
+            self.actionContinue()
+        }) {
+            Text("Continue")
+                .foregroundColor(.white)
+                .font(.system(size: 17, weight: .semibold))
+                .padding(.top, 13)
+                .padding(.bottom, 13)
+                .padding(.leading, 16)
+                .padding(.trailing, 16)
+                .frame(maxWidth: .infinity)
+                .shadow(radius: 10)
         }
+        .modifier(ButtonModifier())
+    }
+}
+
+struct BookProcessView: View {
+    @State private var valueHours: Float = 0.5
+    @State private var valueHumans: Float = 1
+    
+    @State private var numberOfAdults = 1
+    var actionContinue: () -> Void
+    var actionClose: () -> Void
+    
+    var body: some View {
+        Button {
+            self.actionClose()
+        } label: {
+            Image("Close")
+                .padding([.top, .bottom, .trailing], 22)
+                .padding(.leading, 16)
+        }
+        
+        Text("Book a table")
+            .font(.system(size: 32, weight: .bold))
+            .padding(.leading, 16)
+            .padding(.bottom, 22)
+        
+        VStack(alignment: .leading) {
+            Text("Reservation ID")
+                .foregroundColor(Color(UIColor(hex: "#00000080")!))
+                .font(.system(size: 14, weight: .regular))
+            Text("121231")
+                .font(.system(size: 18, weight: .regular))
+        }
+        .padding(.leading, 16)
+        .padding(.bottom, 32)
+        
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Expected date of arrival")
+                    .foregroundColor(Color(UIColor(hex: "#00000080")!))
+                    .font(.system(size: 14, weight: .regular))
+                Text("Today")
+                    .font(.system(size: 18, weight: .regular))
+            }
+            .padding(.leading, 16)
+            Spacer()
+            Button {
+                // Pick Calendar Action
+            } label: {
+                Image("Calendar")
+                    .padding(.trailing, 16)
+            }
+        }
+        .padding(.bottom, 32)
+        
+        VStack(alignment: .leading) {
+            Text("Number of adults")
+                .font(.system(size: 18, weight: .regular))
+                .padding(.bottom, 12)
+            CustomStepperView(value: $valueHumans, valueType: "")
+                .padding(.trailing, 16)
+        }
+        .padding(.leading, 16)
+        .padding(.bottom, 32)
+        
+        VStack(alignment: .leading) {
+            Text("How long will you be dining with us?")
+                .font(.system(size: 18, weight: .regular))
+                .padding(.bottom, 12)
+            CustomStepperView(value: $valueHours, valueType: "hr")
+                .padding(.trailing, 16)
+        }
+        .padding(.leading, 16)
+        .padding(.bottom, 32)
+        
+        VStack(alignment: .leading) {
+            Text("Available reservation times")
+                .font(.system(size: 18, weight: .regular))
+                .padding(.leading, 16)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: -8) {
+                    ForEach(1...10, id: \.self) { object in
+                        Button {
+                            
+                        } label: {
+                        Text("5:00 PM")
+                            .foregroundColor(Color(UIColor(hex: "#00000080")!))
+                            .padding([.top, .bottom], 12)
+                            .padding([.leading, .trailing], 16)
+                            .background(Color(UIColor(hex: "#F6F6F6FF")!))
+                            .cornerRadius(24)
+                            .padding(.leading, 16)
+                        }
+                    }
+                }
+            }
+        }
+        Spacer()
+        Button(action: {
+            self.actionContinue()
+        }) {
+            Text("Continue")
+                .foregroundColor(.white)
+                .font(.system(size: 17, weight: .semibold))
+                .padding(.top, 13)
+                .padding(.bottom, 13)
+                .padding(.leading, 16)
+                .padding(.trailing, 16)
+                .frame(maxWidth: .infinity)
+                .shadow(radius: 10)
+        }
+        .modifier(ButtonModifier())
     }
 }
 
@@ -91,5 +195,48 @@ struct ButtonModifier: ViewModifier {
 struct BookView_Previews: PreviewProvider {
     static var previews: some View {
         BookView()
+    }
+}
+
+struct CustomStepperView: View {
+    @Binding var value: Float
+    var valueType: String
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            Button {
+                if value > 0 && valueType != "hr" {
+                    value -= 1
+                } else if value > 0 && valueType == "hr" {
+                    value -= 0.5
+                }
+            } label: {
+                Image("Minus")
+                    .padding([.top, .bottom], 12)
+                    .padding(.leading, 16)
+            }
+            Spacer()
+            if valueType != "hr" {
+                Text("\(NSString(format: "%.0f", value))")
+                    .font(.system(size: 22, weight: .regular))
+            } else {
+                Text("\(value)" + " " + valueType)
+                    .font(.system(size: 22, weight: .regular))
+            }
+            Spacer()
+            Button {
+                if valueType == "hr" && value < 24 {
+                    value += 0.5
+                } else if valueType != "hr" {
+                    value += 1
+                }
+            } label: {
+                Image("Plus")
+                    .padding([.top, .bottom], 12)
+                    .padding(.trailing, 16)
+            }
+        }
+        .background(Color(UIColor(hex: "#F6F6F6FF")!))
+        .cornerRadius(24)
     }
 }
--- Booking Application/Model/Category.swift
+++ Booking Application/Model/Category.swift
@@ -8,13 +8,33 @@
 import Foundation
 
 struct Category: Codable, Hashable, Identifiable {
-    var id: String
+    var id: String = UUID().uuidString
     var title: String
     var image: String
     
     init(title: String, image: String) {
-        self.id = UUID().uuidString
         self.title = title
         self.image = image
     }
+    
+//    enum CodingKeys: String, CodingKey {
+//        case title = "original_title"
+//        case description = "overview"
+//        case language = "original_language"
+//        case date = "release_date"
+//        case rating = "vote_average"
+//        case posterPath = "poster_path"
+//    }
+//
+//    func getPosterLink() -> String {
+//        return API.posterResource.rawValue + (posterPath ?? "")
+//    }
+//
+//    func getYearReleaseDate() -> String {
+//        return String(date?.prefix(4) ?? "")
+//    }
 }
+
+//struct Results: Codable {
+//    var items: [Movie]
+//}
